b96fa5a87fedc8978ac8c60b2e111126e7bf3908
Henry Burgess <henryjburg@gmail.com>
2022-05-31 13:31:42 -0500

    Fix RDK dot bug

--- src/lib/classes/Dot.ts
+++ src/lib/classes/Dot.ts
@@ -1,5 +1,5 @@
 // Core modules
-import {Renderer} from '../Runtime';
+import { Renderer } from "../Runtime";
 
 /**
  * Dot class used to abstract the positioning of moving dots.
@@ -17,6 +17,7 @@ export class Dot {
   private reference: number;
   private direction: any;
   private dot: any;
+  private stuck: boolean;
   /**
    * Dot class constructor
    * @param {number} x the x cartesian coordinate of the dot
@@ -36,6 +37,7 @@ export class Dot {
     this.reference = parameters.reference;
     this.direction = parameters.direction;
     this.dot = null;
+    this.stuck = false;
   }
 
   /**
@@ -78,25 +80,28 @@ export class Dot {
     let x = this.x;
     let y = this.y;
 
-    if (this.type === 'reference') {
+    if (this.type === "reference") {
       // Check if the dot is currently visible within the circle.
       if (Math.sqrt(x ** 2 + y ** 2) < this.viewRadius) {
         this.shown = true;
       }
 
       // Check if the new coordinates are within region.
-      if (!Renderer.visible(x, y, this.viewRadius) && this.shown === true) {
+      if (
+        !Renderer.visible(x, y, this.viewRadius + this.dotRadius * 2) &&
+        this.shown === true
+      ) {
         x = x - 2 * this.viewRadius * Math.cos(this.direction);
         y = y - 2 * this.viewRadius * Math.sin(this.direction);
         this.shown = false;
       }
-    } else if (this.type === 'random') {
+    } else if (this.type === "random") {
       if (frameCount % 6 === 0) {
         const delta = Math.random();
         if (delta > 0.5) {
-          this.direction -= Math.PI / 8 * delta;
+          this.direction -= (Math.PI / 8) * delta;
         } else {
-          this.direction += Math.PI / 8 * delta;
+          this.direction += (Math.PI / 8) * delta;
         }
       }
 
@@ -109,15 +114,24 @@ export class Dot {
     // Modify the dot position.
     x = x + this.dotVelocity * Math.cos(this.direction);
     y = y + this.dotVelocity * Math.sin(this.direction);
-
     this.x = x;
     this.y = y;
 
     // Perform coordinate translate for renderer, only if dot is within the view
-    const coordinates = Renderer.translate(this.x, this.y,
-        this.width, this.height);
-    if (Renderer.renderable(coordinates[0], coordinates[1],
-        this.width, this.height)) {
+    const coordinates = Renderer.translate(
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    if (
+      Renderer.renderable(
+        coordinates[0],
+        coordinates[1],
+        this.width,
+        this.height
+      )
+    ) {
       this.dot.translation.x = coordinates[0];
       this.dot.translation.y = coordinates[1];
     }
