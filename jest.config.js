5c7c6ca55d7728fa15bc434338241132e0c69b22
Henry Burgess <henryjburg@gmail.com>
2023-04-29 14:40:06 -0500

    Segment testing configuration

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,37 @@
+// Jest configuration
 module.exports = {
-  globals: {
-    __TARGET__: "desktop",
-  },
-  preset: "jest-puppeteer",
-  testMatch: ["**/*.test.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "dist"],
-  moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/test/__mocks__/styles.js",
-  },
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-    "^.+\\.js?$": "ts-jest",
-  },
+  projects: [
+    {
+      displayName: "Unit tests",
+      testMatch: ["<rootDir>/test/classes/**/*.test.ts"],
+      globals: {
+        __TARGET__: "desktop",
+      },
+      testEnvironment: "jsdom",
+      testPathIgnorePatterns: ["/node_modules/", "dist"],
+      moduleNameMapper: {
+        "\\.(css|less)$": "<rootDir>/test/__mocks__/styles.js",
+      },
+      transform: {
+        "^.+\\.ts?$": "ts-jest",
+        "^.+\\.js?$": "ts-jest",
+      },
+    },
+    {
+      displayName: "Integration tests",
+      testMatch: ["<rootDir>/test/integration/**/*.test.ts"],
+      globals: {
+        __TARGET__: "desktop",
+      },
+      preset: "jest-puppeteer",
+      testPathIgnorePatterns: ["/node_modules/", "dist"],
+      moduleNameMapper: {
+        "\\.(css|less)$": "<rootDir>/test/__mocks__/styles.js",
+      },
+      transform: {
+        "^.+\\.ts?$": "ts-jest",
+        "^.+\\.js?$": "ts-jest",
+      },
+    },
+  ]
 };
